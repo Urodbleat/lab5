1b735230d9c1f96462e8fcd170b694b7a439df28
c_worrior <73041418+Urodbleat@users.noreply.github.com>
2024-01-06 03:31:17 +0300

    Update sortings.h

--- sortings.h
+++ sortings.h
@@ -1,6 +1,7 @@
 #ifndef LAB5_2_SORTINGS_H
 #define LAB5_2_SORTINGS_H
 #include <stdlib.h>
+#include "comps.h"
 void defoultSorting(struct Abonent *arr, size_t len, int (*comp)(const void *, const void *));
 void gnomesort(struct Abonent *arr, size_t len, size_t sizeofEl,int (*comp)(const void *, const void *));
 void selectionsort(struct Abonent *arr, size_t len, size_t sizeofEl, int (*comp)(const void *, const void *));
